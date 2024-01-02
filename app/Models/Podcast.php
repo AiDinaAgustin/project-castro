286575068856fc72f80690039c439df4b84f9d2a
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-02 21:47:11 +0700

    Refactor podcast data structure and update routes

--- app/Models/Podcast.php
+++ app/Models/Podcast.php
@@ -7,5 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Podcast extends Model
 {
-    use HasFactory;
+    private static $blog_podcast = [
+        [ 
+            "title" => "Podcast Pertama",
+            "slug" => "podcast-pertama",
+            "author" => "Rizky",
+            "body" => "Lorem ipsum dolor sit amet consectetur"
+        ],
+        [
+            "title" => "Podcast Kedua",
+            "slug" => "podcast-kedua",
+            "author" => "Dina",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at sed magnam ratione, dolorum vero dolorem dicta quisquam non quod perspiciatis nobis impedit obcaecati suscipit sapiente! Et beatae ipsa minima consequuntur similique soluta possimus enim natus, ea rerum molestiae dicta qui obcaecati doloremque delectus iure totam omnis dolor atque tempora ratione dolorum nam. Quisquam voluptates quibusdam dolore rem voluptatibus! Minima, beatae quisquam quam cupiditate in voluptatum non accusamus corrupti ratione sit quasi saepe obcaecati reiciendis, itaque quo molestiae sequi necessitatibus?"
+        ]
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Podcast;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,27 +14,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+$blog_podcast = [
+    [ 
+        "title" => "Podcast Pertama",
+        "slug" => "podcast-pertama",
+        "author" => "Rizky",
+        "body" => "Lorem ipsum dolor sit amet consectetur"
+    ],
+    [
+        "title" => "Podcast Kedua",
+        "slug" => "podcast-kedua",
+        "author" => "Dina",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at sed magnam ratione, dolorum vero dolorem dicta quisquam non quod perspiciatis nobis impedit obcaecati suscipit sapiente! Et beatae ipsa minima consequuntur similique soluta possimus enim natus, ea rerum molestiae dicta qui obcaecati doloremque delectus iure totam omnis dolor atque tempora ratione dolorum nam. Quisquam voluptates quibusdam dolore rem voluptatibus! Minima, beatae quisquam quam cupiditate in voluptatum non accusamus corrupti ratione sit quasi saepe obcaecati reiciendis, itaque quo molestiae sequi necessitatibus?"
+    ],
+    
+];
 
 Route::get('/', function () {
-    $blog_podcast = [
-        [ 
-            "title" => "Podcast Pertama",
-            "slug" => "podcast-pertama",
-            "author" => "Rizky",
-            "body" => "Lorem ipsum dolor sit amet consectetur"
-        ],
-        [
-            "title" => "Podcast Kedua",
-            "slug" => "podcast-kedua",
-            "author" => "Dina",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at sed magnam ratione, dolorum vero dolorem dicta quisquam non quod perspiciatis nobis impedit obcaecati suscipit sapiente! Et beatae ipsa minima consequuntur similique soluta possimus enim natus, ea rerum molestiae dicta qui obcaecati doloremque delectus iure totam omnis dolor atque tempora ratione dolorum nam. Quisquam voluptates quibusdam dolore rem voluptatibus! Minima, beatae quisquam quam cupiditate in voluptatum non accusamus corrupti ratione sit quasi saepe obcaecati reiciendis, itaque quo molestiae sequi necessitatibus?"
-        ],
-        
-    ];
+    
     return view('podcasts', [
         "title" => "Podcast",
-        "podcasts" => $blog_podcast
+        "podcasts" => Podcast::all()
     ]);
 });
 
