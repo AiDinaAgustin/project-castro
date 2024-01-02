32c2e13e60d14334d7fc84c26419e2803b348a05
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-02 21:17:24 +0700

    Add podcast routes and data

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
+    $blog_podcast = [
+        [ 
+            "title" => "Podcast Pertama",
+            "slug" => "podcast-pertama",
+            "author" => "Rizky",
+            "body" => "Lorem ipsum dolor sit amet consectetur"
+        ],
+        [
+            "title" => "Podcast Kedua",
+            "slug" => "podcast-kedua",
+            "author" => "Dina",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at sed magnam ratione, dolorum vero dolorem dicta quisquam non quod perspiciatis nobis impedit obcaecati suscipit sapiente! Et beatae ipsa minima consequuntur similique soluta possimus enim natus, ea rerum molestiae dicta qui obcaecati doloremque delectus iure totam omnis dolor atque tempora ratione dolorum nam. Quisquam voluptates quibusdam dolore rem voluptatibus! Minima, beatae quisquam quam cupiditate in voluptatum non accusamus corrupti ratione sit quasi saepe obcaecati reiciendis, itaque quo molestiae sequi necessitatibus?"
+        ],
+        
+    ];
     return view('podcasts', [
-        "title" => "Podcast"
+        "title" => "Podcast",
+        "podcasts" => $blog_podcast
+    ]);
+});
+
+//halaman single podcast
+Route::get('podcast/{slug}', function ($slug) {
+    return view('podcast', [
+        "title" => "Single Podcast"
     ]);
 });
 
