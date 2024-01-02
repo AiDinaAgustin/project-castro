2e235bc8bdc19545bce368c5a2fc9efef4faebb9
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-02 21:33:55 +0700

    Update podcast links and add single podcast route

--- routes/web.php
+++ routes/web.php
@@ -38,9 +38,36 @@ Route::get('/', function () {
 });
 
 //halaman single podcast
-Route::get('podcast/{slug}', function ($slug) {
+Route::get('/{slug}', function ($slug) {
+    $blog_podcast = [
+        [ 
+            "title" => "Podcast Pertama",
+            "slug" => "podcast-pertama",
+            "author" => "Rizky",
+            "body" => "Lorem ipsum dolor sit amet consectetur"
+        ],
+        [
+            "title" => "Podcast Kedua",
+            "slug" => "podcast-kedua",
+            "author" => "Dina",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at sed magnam ratione, dolorum vero dolorem dicta quisquam non quod perspiciatis nobis impedit obcaecati suscipit sapiente! Et beatae ipsa minima consequuntur similique soluta possimus enim natus, ea rerum molestiae dicta qui obcaecati doloremque delectus iure totam omnis dolor atque tempora ratione dolorum nam. Quisquam voluptates quibusdam dolore rem voluptatibus! Minima, beatae quisquam quam cupiditate in voluptatum non accusamus corrupti ratione sit quasi saepe obcaecati reiciendis, itaque quo molestiae sequi necessitatibus?"
+        ],
+        
+    ];
+
+    //buat variable $new_podcast untuk menampung data podcast yang akan diambil
+    $new_podcast = [];
+
+    //lakukan perulangan untuk mencari data podcast yang sesuai dengan slug
+    foreach($blog_podcast as $podcast){
+        if($podcast["slug"] === $slug){
+            $new_podcast = $podcast;
+        }
+    }
+
     return view('podcast', [
-        "title" => "Single Podcast"
+        "title" => "Single Podcast",
+        "podcast" => $new_podcast
     ]);
 });
 
