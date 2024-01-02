72e1ce8e686909c7137fbab20ea73702322655ce
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-02 21:53:24 +0700

    Refactor Podcast model to use static data source

--- app/Models/Podcast.php
+++ app/Models/Podcast.php
@@ -2,10 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Podcast extends Model
+class Podcast 
 {
     private static $blog_podcast = [
         [ 
@@ -21,4 +19,9 @@ class Podcast extends Model
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at sed magnam ratione, dolorum vero dolorem dicta quisquam non quod perspiciatis nobis impedit obcaecati suscipit sapiente! Et beatae ipsa minima consequuntur similique soluta possimus enim natus, ea rerum molestiae dicta qui obcaecati doloremque delectus iure totam omnis dolor atque tempora ratione dolorum nam. Quisquam voluptates quibusdam dolore rem voluptatibus! Minima, beatae quisquam quam cupiditate in voluptatum non accusamus corrupti ratione sit quasi saepe obcaecati reiciendis, itaque quo molestiae sequi necessitatibus?"
         ]
     ];
+
+    public static function all()
+    {
+        return self::$blog_podcast;
+    }
 }
