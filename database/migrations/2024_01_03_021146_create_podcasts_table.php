ffbfca8e44314214039472b41c53a754527a95d0
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-03 09:22:14 +0700

    Add title, body, and published_at columns to podcasts table

--- database/migrations/2024_01_03_021146_create_podcasts_table.php
+++ database/migrations/2024_01_03_021146_create_podcasts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('podcasts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body'); 
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
