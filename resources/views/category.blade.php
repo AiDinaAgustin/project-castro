4c0f8ce7153e136006050e4f304e1ab10e8b1760
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-04 22:05:51 +0700

    Update podcast cover image URL

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -15,7 +15,7 @@
                 <a href="/podcasts/{{ $podcast->slug }}">
                   <div class="cover-music bg-zinc-800 w-40 h-56 rounded-md p-2">
                     <div class="relative w-36">
-                      <img class="w-full" src="/assets/img/page1.jpg" alt="your music">
+                      <img class="w-full" src="https://source.unsplash.com/500x270/?{{ $podcast->category->name }}" alt="your music">
                       <div class="play-btn">
                         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
                       </div>
