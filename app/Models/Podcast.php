0dbaf39a4648033138e0c94ea378c4cb5aaccb14
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-04 21:14:20 +0700

    Refactor author relationship in Podcast model and view
    This commit refactors the `author` relationship in the `Podcast` model and view. The relationship has been changed from `user` to `author` for better clarity and understanding. Additionally, the `belongsTo` method in the model has been updated to search for `user_id` in the `users` table.

--- app/Models/Podcast.php
+++ app/Models/Podcast.php
@@ -16,8 +16,8 @@ class Podcast extends Model
         return $this->belongsTo(Category::class);
     }
 
-    public function author()
+    public function author() //ini diubah dari user menjadi author agar lebih mudah dipahami, karena user disini adalah author
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id'); //pengubahan ini dilakukan agar sesuai saja dengan author yang dicari, karena secara default laravel akan mencari user_id di tabel users
     }
 }
