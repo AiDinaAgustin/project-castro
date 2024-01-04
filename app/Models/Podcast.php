0351eb59d2b3060c547109b46589e1a3dc8a3971
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-04 15:16:34 +0700

    Add relationship between Podcast and User models

--- app/Models/Podcast.php
+++ app/Models/Podcast.php
@@ -15,4 +15,9 @@ class Podcast extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
