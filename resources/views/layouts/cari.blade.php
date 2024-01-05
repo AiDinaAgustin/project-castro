9af2e08fc9ac2c53fbcdfd259c06fba6046a9737
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-05 15:19:46 +0700

    fiks cari

--- resources/views/layouts/cari.blade.php
+++ resources/views/layouts/cari.blade.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="shortcut icon" href="/assets/img/LOGO CASTRO (2) 2.png" type="image/x-icon">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <!-- Link Swiper's CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -42,31 +43,42 @@
       <div class="overflow-hidden flex-1 bg-zinc-900 rounded-lg mx-auto p-4">
         <header class="sticky top-0 bg-zinc-900 z-50 flex items-center justify-between mb-4 py-2">
           <div class="button-nav flex items-center">
-            <div class="btn-undo mr-4">
-              <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" fill="#F9D80F"><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/></svg>
-            </div>
-            <div class="btn-redo mr-4">
+            {{-- <div class="btn-undo mr-4">
+                <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" fill="#F9D80F"><path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"/></svg>
+              </div> --}}
+            {{-- <div class="btn-redo mr-4">
               <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512" fill="#F9D80F"><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/></svg>
+            </div> --}}
+              {{-- <form action="/">
+                <div class="relative search-btn w-72 h-8 bg-black rounded-full overflow-hidden px-4">
+                  <div class="absolute translate-y-2/4 top-0 left-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="white"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                  </div>
+                    <input class="w-full h-full ml-5 text-white bg-transparent outline-none" type="text" placeholder="What do you want to listen?" name="search">
+                </div>
+            </form> --}}
+            <div class="row">
+              
+              <div class="col-lg-3"></div>
+                  <div class="col-md-12">
+                      <div class="form-group">
+                        <div class="relative search-btn w-72 h-8 bg-black rounded-full overflow-hidden px-6">
+                          <a href="/cari">
+                            <div class="absolute translate-y-2/4 top-0 left-4">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+                              </svg>
+                            </div>
+                          </a>
+                            <input class="w-full h-full ml-5 text-white bg-transparent outline-none" type="text" name="search" id="search" placeholder="What do you want to listen?" onfocus="this.value=''">
+                        </div>
+                        {{-- <input type="text" name="search" id="search" placeholder="Enter search name" class="form-control" onfocus="this.value=''"> --}}
+                      </div>
+                      <div id="search_list"></div>
+                  </div>
+              <div class="col-lg-3"></div>
             </div>
-            {{-- <form action="/">
-              <div class="relative search-btn w-72 h-8 bg-black rounded-full overflow-hidden px-4">
-                <div class="absolute translate-y-2/4 top-0 left-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="white"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
-                </div>
-                  <input class="w-full h-full ml-5 text-white bg-transparent outline-none" type="text" placeholder="What do you want to listen?" name="search">
-              </div>
-          </form> --}}
-          <div class="row">
-            <div class="col-lg-3"></div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                        <input type="text" name="search" id="search" placeholder="Enter search name" class="form-control" onfocus="this.value=''">
-                    </div>
-                    <div id="search_list"></div>
-                </div>
-            <div class="col-lg-3"></div>
           </div>
-            </div>
         </header>
         
         <main class="w-full min-h-screen overflow-x-hidden">
