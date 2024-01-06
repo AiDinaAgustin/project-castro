aa67884e19abee919db5cfbbff2fc1e3633faa8c
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-06 11:23:25 +0700

    Add category and podcast seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,21 +23,21 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // Category::create(
-        //     [
-        //         'name' => 'Misteri',
-        //         'slug' => 'misteri'
-        //     ]
-        // );
+        Category::create(
+            [
+                'name' => 'Misteri',
+                'slug' => 'misteri'
+            ]
+        );
 
-        // Category::create(
-        //     [
-        //         'name' => 'Horror',
-        //         'slug' => 'horror'
-        //     ]
-        // );
+        Category::create(
+            [
+                'name' => 'Supernatural',
+                'slug' => 'supernatural'
+            ]
+        );
 
-        // Podcast::factory(10)->create();
+        Podcast::factory(20)->create();
 
         // User::create(
         //     [
