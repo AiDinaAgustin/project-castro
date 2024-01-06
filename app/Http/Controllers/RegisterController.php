6ed23f4f6bb08f26db00c650447c87feb5b6cbb8
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-06 12:05:54 +0700

    Add form validation to RegisterController.php

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Carbon\Carbon;
 
 use Illuminate\Http\Request;
 
@@ -13,8 +14,17 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
-       return request()->all();
+       $request->validate([
+           'name' => 'required|max:255',
+           'username' => 'required|min:3|max:255|unique:users',
+           'email' => 'required|email:dns|unique:users',
+           'password' => 'required|min:5|max:255',
+           'gender' => 'required|in:MALE,FEMALE',
+           'birth' => 'required|date|before_or_equal:' . Carbon::now()->subYears(13)->format('m-d-Y')
+       ]);
+
+       dd('success');
     }
 }
