0e40fe0e1a54e1b317648c5b5a8f68fc166654f8
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-08 05:28:10 +0700

    Update table headers in podcast index view

--- resources/views/dashboard/podcasts/index.blade.php
+++ resources/views/dashboard/podcasts/index.blade.php
@@ -97,11 +97,11 @@
         <table class="table table-striped table-sm">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
-              <th scope="col">Header</th>
+              <th scope="col">No</th>
+              <th scope="col">Title</th>
+              <th scope="col">Category</th>
+              <th scope="col">Tanggal</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
