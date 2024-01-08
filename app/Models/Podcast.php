6d9fdf081422c3ab0a8037f5299f106b13d79092
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-08 09:55:14 +0700

    Add Sluggable trait to Podcast model and update composer.json

--- app/Models/Podcast.php
+++ app/Models/Podcast.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Podcast extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guarded = ['id'];
 
@@ -24,5 +25,14 @@ class Podcast extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
