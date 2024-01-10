d2ec15e76cc97822390ded7f49e686f7f346cecf
AiDinaAgustin <aidinagustin2@gmail.com>
2024-01-10 09:53:59 +0700

    Add podcast image upload functionality

--- resources/views/dashboard/podcasts/edit.blade.php
+++ resources/views/dashboard/podcasts/edit.blade.php
@@ -35,6 +35,14 @@
               </select>
         </div>
         <div class="mb-3">
+            <label for="image" class="form-label">Podcast Image</label>
+            <img class="img-preview img-fluid mb-3 col-sm-5">
+            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+            @error('image')
+                {{ $message }}
+            @enderror
+        </div>
+        <div class="mb-3">
             <label for="category" class="form-label">Body</label>
             @error('body')
                 <p class="text-danger">{{ $message }}</p>
@@ -59,6 +67,20 @@
     document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
     })
+
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+
+        imgPreview.style.display = 'block';
+
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(image.files[0]);
+
+        oFReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
 </script>
 
 @endsection
